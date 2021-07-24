a729f61ced8f3ccfba59baf685dcdda46c451b8d
hdw0504 <768685351@qq.com>
2021-07-24 23:31:28 +0800

    增加全局组件

---
+++ src/components/dwButton/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <button :class="[type, loading ? 'loading' : '', waiting ? 'waiting' : '']">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "dwButton",
+  props: {
+    // primary success info warning danger
+    type: {
+      type: String,
+      default: "default",
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+    waiting: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style scoped>
+@import "./assets/index.css";
+@import "./assets/status.css";
+</style>
